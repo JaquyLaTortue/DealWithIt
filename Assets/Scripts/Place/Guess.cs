63d818e7b7fda8c0fadf78e200e16ff767660c4f
JaquyLaTortue <93046036+JaquyLaTortue@users.noreply.github.com>
2023-12-13 15:30:47 +0100

    Hider & Seeker created + start of guess

---
+++ Assets/Scripts/Place/Guess.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guess : MonoBehaviour
+{
+    [SerializeField] LayerMask Props;
+    [SerializeField] LayerMask Target;
+
+    void OnGuess()
+    {
+
+    }
+}
