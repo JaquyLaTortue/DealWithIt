2838209c484d3df05460d52beddce565a4d56206
JaquyLaTortue <93046036+JaquyLaTortue@users.noreply.github.com>
2023-12-14 15:23:41 +0100

    DotWeen  added + Map scene created with the IKEO

--- Assets/Scripts/Controls/Move.cs
+++ Assets/Scripts/Controls/Move.cs
@@ -1,36 +1,50 @@
+using Palmmedia.ReportGenerator.Core.CodeAnalysis;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
     //Define at wich speed the player moves
+    [Header("Speed")]
     public float InitialSpeed;
+    private float Speed;
 
-    [SerializeField] private float Speed;
+    [Header("Axis")]
+    private float _horizontalInput;
+    private float _verticalInput;
+    Vector3 _moveDirection;
+
+    [Header("References")]
+    private Rigidbody rb;
+ 
+    //Stores the value of the move input
 
     private void Start()
     {
         Speed = InitialSpeed;
+        rb = GetComponent<Rigidbody>();
     }
     public void SetSneakSpeed(float desiredSpeed)
     {
         Speed = desiredSpeed;
     }
 
-    //Stores the value of the move input
-    Vector3 _moveDirection;
 
     //gets the value of the move input
     public void OnMove(InputAction.CallbackContext context)
     {
-        _moveDirection = context.ReadValue<Vector2>();
+        _horizontalInput = context.ReadValue<Vector2>().x;
+        _verticalInput = context.ReadValue<Vector2>().y;
     }
 
     //will update the player position by the move input
     void Update()
     {
-        Vector3 moveDirection = new Vector3(_moveDirection.x, 0, _moveDirection.y);
-        transform.Translate(moveDirection * (Speed * Time.deltaTime));
+        //Vector3 moveDirection = new Vector3(_horizontalInout, 0, _verticalInput);
+        //transform.Translate(moveDirection * (Speed * Time.deltaTime));
+        
+        _moveDirection = transform.forward * _verticalInput + transform.right * _horizontalInput;
+        rb.AddForce(_moveDirection.normalized * Speed * 10f, ForceMode.Force);
 
     }
 }
