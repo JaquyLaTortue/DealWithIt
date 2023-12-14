8dc5c92ac889c5ff4bcf0e73ddc6526285ef7013
Your Name <you@example.com>
2023-12-14 09:49:24 +0100

    Continue du sneak

--- Assets/Scripts/Controls/Sneak.cs
+++ Assets/Scripts/Controls/Sneak.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Interactions;
-using UnityEngine.Windows;
 
 
 public class Sneak : MonoBehaviour
@@ -11,32 +8,25 @@ public class Sneak : MonoBehaviour
     Rigidbody rb;
     public bool Crouch { get; private set; }
 
-    public float sneak = 2f;
+    public float sneak = 0.5f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    private InputAction _crouchAction;
-
-    // Start is called before the first frame update
-    void Start()
+    public void OnSneak(InputAction.CallbackContext ctx)
     {
-        
-    }
+        if (ctx.started)
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void Sneaking(InputAction.CallbackContext ctx)
-    {
-        if (!ctx.started) return;
-        Debug.Log("ah");
-        rb.position = Vector3.up / sneak;
+            //gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - sneak, gameObject.transform.position.z);
+            gameObject.transform.localScale = new Vector3(1, sneak, 1);
+        }
+        else if (ctx.canceled)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
         
     }
 }
