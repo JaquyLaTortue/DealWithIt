20701ed1dca1de5835574ef68de087246f6578a4
Your Name <you@example.com>
2023-12-15 09:43:30 +0100

    Fin Choix Random de l'endroit de où il faut mettre

--- Assets/Scripts/ObjectPlace/ObjectPlace.cs
+++ Assets/Scripts/ObjectPlace/ObjectPlace.cs
@@ -11,15 +11,21 @@ public class ObjectPlace : MonoBehaviour
 
 
     List<string> Color = new() {"Green" , "Red", "Yellow", "Bue", "Purple", "Orange", "Cyan", "White", "Black", "Grey"};
-    List<string> Adjectif = new() {"Stable", "reachable", "Heavy", "Beautifull"};
-    List<string> Forme = new() {"Round", "Square", "Triangle", "Star"};
+    List<string> Adjectif = new() {"Stable", "Reachable", "Heavy", "Unreachable", "Tough", "Light", "Wood", "Paper", "Carpet", "Iron"};
+    List<string> Forme = new() {"Round", "Square", "Triangle", "Star", "Long", "Big", "Small", "Short", "Oval", "Medium"};
+
+    int ChoixRandom()
+    {
+        int Choix = Random.Range(0, 9);
+        return Choix;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        //ColorText.text = ();
-        //AdjectifText.text = ();
-        //FormeText.text = ();
+        ColorText.text = ($"- {Color[ChoixRandom()]}");
+        AdjectifText.text = ($"- {Adjectif[ChoixRandom()]}");
+        FormeText.text = ($"- {Forme[ChoixRandom()]}");
     }
 
     // Update is called once per frame
