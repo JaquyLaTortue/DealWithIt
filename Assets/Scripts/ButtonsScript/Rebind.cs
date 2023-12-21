f2151900692e77c71e12cb08e87acf0b996e9b8a
JaquyLaTortue <93046036+JaquyLaTortue@users.noreply.github.com>
2023-12-21 11:45:44 +0100

    Add rebind open&close + camera clip fixed

--- Assets/Scripts/ButtonsScript/Rebind.cs
+++ Assets/Scripts/ButtonsScript/Rebind.cs
@@ -4,19 +4,35 @@ using UnityEngine.InputSystem;
 
 public class Rebind : MonoBehaviour
 {
-    [Header("Rebind Text")]
+    [Header("Hider Rebind Text")]
     [SerializeField] TMP_Text placeRebindText;
     [SerializeField] TMP_Text cancelPlacementRebindText;
     [SerializeField] TMP_Text validatePlacementRebindText;
     [SerializeField] TMP_Text ChangePlacementRebindText;
-    [SerializeField] TMP_Text guessRebindText;
+    [SerializeField] TMP_Text OpenHiderText;
+    [SerializeField] TMP_Text CloseHiderText;
+    [SerializeField] TMP_Text CrouchHiderText;
 
-    [Header("Input Actions")]
+    [Header("Seeker Rebind Text")]
+    [SerializeField] TMP_Text guessRebindText;
+    [SerializeField] TMP_Text OpenSeekerText;
+    [SerializeField] TMP_Text CloseSeekerText;
+    [SerializeField] TMP_Text CrouchSeeekerText;
+
+    [Header("Hider Input Actions")]
     public InputActionReference _placeAction;
     public InputActionReference _cancelPlacementAction;
     public InputActionReference _validatePlacementAction;
     public InputActionReference _changePlacementAction;
+    public InputActionReference _openHiderAction;
+    public InputActionReference _closeHiderAction;
+
+
+    [Header("Seeker Input Actions")]
     public InputActionReference _guessAction;
+    public InputActionReference _openSeekerAction;
+    public InputActionReference _closeSeekerAction;
+
 
     [Header("Rebind UI")]
     [SerializeField] GameObject RebindUI;
@@ -46,10 +62,8 @@ public class Rebind : MonoBehaviour
         _binding.overridePath = rebindingOperation.action.bindings[0].effectivePath;
         _action.ApplyBindingOverride(0, _binding);
 
-        //Debug.Log(rebindingOperation.action.bindings[0].effectivePath);
         _action.Enable();
         rebindingOperation.Dispose();
-        //Debug.Log(InputControlPath.ToHumanReadableString(_action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice));
         UpdateText();
         RebindUI.SetActive(false);
     }
@@ -65,5 +79,13 @@ public class Rebind : MonoBehaviour
         ChangePlacementRebindText.text = $"Change Placement   Mode : {InputControlPath.ToHumanReadableString(_changePlacementAction.action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice)}";
 
         guessRebindText.text = $"Guess : {InputControlPath.ToHumanReadableString(_guessAction.action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice)}";
+
+        OpenHiderText.text = $"Open for Hider : {InputControlPath.ToHumanReadableString(_openHiderAction.action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice)}";
+
+        CloseHiderText.text = $"Close for Hider : {InputControlPath.ToHumanReadableString(_closeHiderAction.action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice)}";
+
+        OpenSeekerText.text = $"Open for Seeker : {InputControlPath.ToHumanReadableString(_openSeekerAction.action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice)}";
+
+        CloseSeekerText.text = $"Close for Seeker : {InputControlPath.ToHumanReadableString(_closeSeekerAction.action.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice)}";
     }
 }
